8167f5a7128fe769c4616b288fbc5ca35d538043
OverMan-647 <brunoadji@id.uff.br>
2023-05-14 00:11:49 -0300

    Loading countries

--- src/model/map/territories/continent.js
+++ src/model/map/territories/continent.js
@@ -1,12 +1,25 @@
 import "/country.js";
 
 class Continent {
-    constructor(name) {
+    constructor(name, bonus) {
         this.name = name;
         this.countries = [];
+        this.bonus = bonus;
     }
 
     addCountry(country) {
         this.countries.push(country);
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getCountries() {
+        return this.countries;
+    }
+
+    getBonus() {
+        return this.bonus;
     }
 }
--- src/model/map/territories/country.js
+++ src/model/map/territories/country.js
@@ -1,17 +1,50 @@
 import "/continent.js";
 
 class Country {
-    constructor(name, path, continent) {
+    constructor(name, path, continent, neighbors) {
         this.name = name;
         this.path = path;
-        this.neighbors = [];
+        this.neighbors = neighbors;
         this.owner = null;
         this.continent = null;
-        this.addContinent(continent);
+        this.setContinent(continent);
+        this.soldiers = 0;
+    }
+    
+    getName() {
+        return this.name;
+    }
+    
+    getPath() {
+        return this.path;
+    }
+    
+    getNeighbors() {
+        return this.neighbors;
+    }
+    
+    getOwner() {
+        return this.owner;
+    }
+    
+    getContinent() {
+        return this.continent;
     }
 
-    addContinent(continent) {
+    getSoldiers() {
+        return this.soldiers;
+    }
+    
+    setContinent(continent) {
         this.continent = continent;
         continent.addCountry(this);
+    }
+
+    setOwner(newOwner) {
+        //todo (Conquista de territorio)
+    }
+
+    setSoldiers(soldiers) {
+        this.soldiers = soldiers;
     }
 }
