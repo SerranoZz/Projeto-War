9beb46d36821da98df616314450835320e8b21cc
Lucas Serrano <lucas_serrano@id.uff.br>
2023-05-31 16:45:54 -0300

    Adding America and Africa

--- src/main.js
+++ src/main.js
@@ -75,10 +75,10 @@ class Game{
 
     async #createGameScreen(){
         //init
-        const mapa = new ImageGL();
-        await mapa.init(this.gl, "./assets/mapa.jpg");
+        //const mapa = new ImageGL();
+        //await mapa.init(this.gl, "./assets/mapa.jpg");
 
-        mapa.scale = [2.7, 2.7];
+        //mapa.scale = [2.7, 2.7];
 
         const gameScreen = new GameScreen();
         await gameScreen.init(this.gl);
@@ -89,33 +89,123 @@ class Game{
         const fortify = new Fortify();
         await fortify.init(this.gl);
     
+        //South America
         const brasil = await IndexedMeshT.loadMeshFromObj(
-            "./assets/meshes/brasil-rotacionado.obj", 
+            "./assets/meshes/south_america/brasil.obj", 
             this.gl, countryVert, phongFrag
         );
-        brasil.scale = [1.2, 1.2, 1];
-        brasil.position = [0*-1.2, 0*-0.9, 0.3];
-        brasil.rotation[1] = -0.2;
+        brasil.scale = [4.15, 4.15, 1];
     
         const argentina = await IndexedMeshT.loadMeshFromObj(
-            "./assets/meshes/argentina.obj", 
+            "./assets/meshes/south_america/argentina.obj", 
             this.gl, countryVert, phongFrag
         );
-        argentina.scale = [1.6, 1.6, 1.5];
-        argentina.position = [-0.21, -0.35, 0.3];
+        argentina.scale = [4.15, 4.15, 1];
 
-        const peru = await IndexedMeshT.loadMeshFromObj(
-            "./assets/meshes/peru.obj",
+        const chile = await IndexedMeshT.loadMeshFromObj(
+            "./assets/meshes/south_america/chile.obj", 
             this.gl, countryVert, phongFrag
-        )
-        peru.scale = [6, 6, 1.5];
+        );
+        chile.scale = [4.15, 4.15, 1];
 
         const venezuela = await IndexedMeshT.loadMeshFromObj(
-            "./assets/meshes/venezuela.obj",
+            "./assets/meshes/south_america/venezuela.obj", 
             this.gl, countryVert, phongFrag
-        )
-        venezuela.scale = [6, 6, 1.5];
+        );
+        venezuela.scale = [4.15, 4.15, 1];
+
+        //North America
+        const mexico = await IndexedMeshT.loadMeshFromObj(
+            "./assets/meshes/north_america/mexico.obj", 
+            this.gl, countryVert, phongFrag
+        );
+        mexico.scale = [4.15, 4.15, 1];
+
+        const california = await IndexedMeshT.loadMeshFromObj(
+            "./assets/meshes/north_america/california.obj", 
+            this.gl, countryVert, phongFrag
+        );
+        california.scale = [4.15, 4.15, 1];
+
+        const nova_york = await IndexedMeshT.loadMeshFromObj(
+            "./assets/meshes/north_america/nova_york.obj", 
+            this.gl, countryVert, phongFrag
+        );
+        nova_york.scale = [4.15, 4.15, 1];
     
+        const ottawa = await IndexedMeshT.loadMeshFromObj(
+            "./assets/meshes/north_america/ottawa.obj", 
+            this.gl, countryVert, phongFrag
+        );
+        ottawa.scale = [4.15, 4.15, 1];
+
+        const labrador = await IndexedMeshT.loadMeshFromObj(
+            "./assets/meshes/north_america/labrador.obj", 
+            this.gl, countryVert, phongFrag
+        );
+        labrador.scale = [4.15, 4.15, 1];
+
+        const vancouver = await IndexedMeshT.loadMeshFromObj(
+            "./assets/meshes/north_america/vancouver.obj", 
+            this.gl, countryVert, phongFrag
+        );
+        vancouver.scale = [4.15, 4.15, 1];
+
+        const alaska = await IndexedMeshT.loadMeshFromObj(
+            "./assets/meshes/north_america/alaska.obj", 
+            this.gl, countryVert, phongFrag
+        );
+        alaska.scale = [4.15, 4.15, 1];
+
+        const mackenzie = await IndexedMeshT.loadMeshFromObj(
+            "./assets/meshes/north_america/mackenzie.obj", 
+            this.gl, countryVert, phongFrag
+        );
+        mackenzie.scale = [4.15, 4.15, 1];
+
+        const groelandia = await IndexedMeshT.loadMeshFromObj(
+            "./assets/meshes/north_america/groelandia.obj", 
+            this.gl, countryVert, phongFrag
+        );
+        groelandia.scale = [4.15, 4.15, 1];
+
+        //Africa
+        const africa_sul = await IndexedMeshT.loadMeshFromObj(
+            "./assets/meshes/africa/africa_sul.obj", 
+            this.gl, countryVert, phongFrag
+        );
+        africa_sul.scale = [4.15, 4.15, 1];
+
+        const argelia = await IndexedMeshT.loadMeshFromObj(
+            "./assets/meshes/africa/argelia.obj", 
+            this.gl, countryVert, phongFrag
+        );
+        argelia.scale = [4.15, 4.15, 1];
+
+        const congo = await IndexedMeshT.loadMeshFromObj(
+            "./assets/meshes/africa/congo.obj", 
+            this.gl, countryVert, phongFrag
+        );
+        congo.scale = [4.15, 4.15, 1];
+
+        const egito = await IndexedMeshT.loadMeshFromObj(
+            "./assets/meshes/africa/egito.obj", 
+            this.gl, countryVert, phongFrag
+        );
+        egito.scale = [4.15, 4.15, 1];
+
+        const madagascar = await IndexedMeshT.loadMeshFromObj(
+            "./assets/meshes/africa/madagascar.obj", 
+            this.gl, countryVert, phongFrag
+        );
+        madagascar.scale = [4.15, 4.15, 1];
+
+        const sudao = await IndexedMeshT.loadMeshFromObj(
+            "./assets/meshes/africa/sudao.obj", 
+            this.gl, countryVert, phongFrag
+        );
+        sudao.scale = [4.15, 4.15, 1];
+
         this.#gameScene = new Scene(this.gl);
         this.#gameScene.createCamera(canvas);
         this.#gameScene.camera.camPosition[2] = 1.8;
@@ -145,25 +235,85 @@ class Game{
         cImage.scale = [0.1, 0.1];
         cImage.positionY = 0.2;
     
-        this.#gameScene.appendElement(mapa, brasil, argentina, peru, venezuela, cImage);
+        this.#gameScene.appendElement(brasil, argentina, chile, venezuela, mexico, california, nova_york, ottawa, labrador, vancouver, alaska, mackenzie, groelandia, africa_sul, argelia, congo, egito, madagascar, sudao, cImage);
         this.#guiScene.appendElement(gameScreen, show_cards, fortify);
     
         //colocar a view e a projection
         brasil.setUniformValue("view", this.#gameScene.camera.viewMatrix, "Matrix4fv");
         brasil.setUniformValue("projection", this.#gameScene.camera.projMatrix, "Matrix4fv");
-        brasil.setUniformValue("color", [1.0, 0.0, 0.0, 1.0], "4fv");
+        brasil.setUniformValue("color", [0.0, 0.0, 1.0, 1.0], "4fv");
     
         argentina.setUniformValue("view", this.#gameScene.camera.viewMatrix, "Matrix4fv");
         argentina.setUniformValue("projection", this.#gameScene.camera.projMatrix, "Matrix4fv");
         argentina.setUniformValue("color", [0.0, 0.0, 1.0, 1.0], "4fv");
 
-        peru.setUniformValue("view", this.#gameScene.camera.viewMatrix, "Matrix4fv");
-        peru.setUniformValue("projection", this.#gameScene.camera.projMatrix, "Matrix4fv");
-        peru.setUniformValue("color", [0.0, 1.0, 0.0, 1.0], "4fv");
-        
+        chile.setUniformValue("view", this.#gameScene.camera.viewMatrix, "Matrix4fv");
+        chile.setUniformValue("projection", this.#gameScene.camera.projMatrix, "Matrix4fv");
+        chile.setUniformValue("color", [0.0, 0.0, 1.0, 1.0], "4fv");
+
         venezuela.setUniformValue("view", this.#gameScene.camera.viewMatrix, "Matrix4fv");
         venezuela.setUniformValue("projection", this.#gameScene.camera.projMatrix, "Matrix4fv");
-        venezuela.setUniformValue("color", [0.0, 1.0, 0.0, 1.0], "4fv");
+        venezuela.setUniformValue("color", [0.0, 0.0, 1.0, 1.0], "4fv");
+
+        mexico.setUniformValue("view", this.#gameScene.camera.viewMatrix, "Matrix4fv");
+        mexico.setUniformValue("projection", this.#gameScene.camera.projMatrix, "Matrix4fv");
+        mexico.setUniformValue("color", [0.0, 0.0, 1.0, 1.0], "4fv");
+
+        california.setUniformValue("view", this.#gameScene.camera.viewMatrix, "Matrix4fv");
+        california.setUniformValue("projection", this.#gameScene.camera.projMatrix, "Matrix4fv");
+        california.setUniformValue("color", [0.0, 0.0, 1.0, 1.0], "4fv");
+
+        nova_york.setUniformValue("view", this.#gameScene.camera.viewMatrix, "Matrix4fv");
+        nova_york.setUniformValue("projection", this.#gameScene.camera.projMatrix, "Matrix4fv");
+        nova_york.setUniformValue("color", [0.0, 0.0, 1.0, 1.0], "4fv");
+
+        ottawa.setUniformValue("view", this.#gameScene.camera.viewMatrix, "Matrix4fv");
+        ottawa.setUniformValue("projection", this.#gameScene.camera.projMatrix, "Matrix4fv");
+        ottawa.setUniformValue("color", [0.0, 0.0, 1.0, 1.0], "4fv");
+
+        labrador.setUniformValue("view", this.#gameScene.camera.viewMatrix, "Matrix4fv");
+        labrador.setUniformValue("projection", this.#gameScene.camera.projMatrix, "Matrix4fv");
+        labrador.setUniformValue("color", [0.0, 0.0, 1.0, 1.0], "4fv");
+
+        vancouver.setUniformValue("view", this.#gameScene.camera.viewMatrix, "Matrix4fv");
+        vancouver.setUniformValue("projection", this.#gameScene.camera.projMatrix, "Matrix4fv");
+        vancouver.setUniformValue("color", [0.0, 0.0, 1.0, 1.0], "4fv");
+
+        alaska.setUniformValue("view", this.#gameScene.camera.viewMatrix, "Matrix4fv");
+        alaska.setUniformValue("projection", this.#gameScene.camera.projMatrix, "Matrix4fv");
+        alaska.setUniformValue("color", [0.0, 0.0, 1.0, 1.0], "4fv");
+
+        mackenzie.setUniformValue("view", this.#gameScene.camera.viewMatrix, "Matrix4fv");
+        mackenzie.setUniformValue("projection", this.#gameScene.camera.projMatrix, "Matrix4fv");
+        mackenzie.setUniformValue("color", [0.0, 0.0, 1.0, 1.0], "4fv");
+
+        groelandia.setUniformValue("view", this.#gameScene.camera.viewMatrix, "Matrix4fv");
+        groelandia.setUniformValue("projection", this.#gameScene.camera.projMatrix, "Matrix4fv");
+        groelandia.setUniformValue("color", [0.0, 0.0, 1.0, 1.0], "4fv");
+
+        africa_sul.setUniformValue("view", this.#gameScene.camera.viewMatrix, "Matrix4fv");
+        africa_sul.setUniformValue("projection", this.#gameScene.camera.projMatrix, "Matrix4fv");
+        africa_sul.setUniformValue("color", [0.0, 0.0, 1.0, 1.0], "4fv");
+
+        argelia.setUniformValue("view", this.#gameScene.camera.viewMatrix, "Matrix4fv");
+        argelia.setUniformValue("projection", this.#gameScene.camera.projMatrix, "Matrix4fv");
+        argelia.setUniformValue("color", [0.0, 0.0, 1.0, 1.0], "4fv");
+
+        congo.setUniformValue("view", this.#gameScene.camera.viewMatrix, "Matrix4fv");
+        congo.setUniformValue("projection", this.#gameScene.camera.projMatrix, "Matrix4fv");
+        congo.setUniformValue("color", [0.0, 0.0, 1.0, 1.0], "4fv");
+
+        egito.setUniformValue("view", this.#gameScene.camera.viewMatrix, "Matrix4fv");
+        egito.setUniformValue("projection", this.#gameScene.camera.projMatrix, "Matrix4fv");
+        egito.setUniformValue("color", [0.0, 0.0, 1.0, 1.0], "4fv");
+
+        madagascar.setUniformValue("view", this.#gameScene.camera.viewMatrix, "Matrix4fv");
+        madagascar.setUniformValue("projection", this.#gameScene.camera.projMatrix, "Matrix4fv");
+        madagascar.setUniformValue("color", [0.0, 0.0, 1.0, 1.0], "4fv");
+
+        sudao.setUniformValue("view", this.#gameScene.camera.viewMatrix, "Matrix4fv");
+        sudao.setUniformValue("projection", this.#gameScene.camera.projMatrix, "Matrix4fv");
+        sudao.setUniformValue("color", [0.0, 0.0, 1.0, 1.0], "4fv");
 
         this.gl.canvas.addEventListener("click", e=>{
             // e.clientX e e.clientY são a posição do mouse
