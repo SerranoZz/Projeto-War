e6ad99d086214cce58628bfbd1a8c5f2036f4e5a
brunoadji <brunoadji@id.uff.br>
2023-06-28 22:30:10 -0300

    Adding few more functions

--- src/AI/AI.js
+++ src/AI/AI.js
@@ -2,4 +2,8 @@ class AI {
     constructor(game) {
         this.#currentState = new Board(game, 0);
     }
+
+    play() {
+        return this.#currentState.bestBoard();
+    }
 }
--- src/AI/board.js
+++ src/AI/board.js
@@ -1,11 +1,26 @@
 class Board {
 
-    constructor(game, depthLevel) {
-        game.clone();
+    constructor(game, depthLevel, player) {
+        this.#game = game.clone();
         this.#depthLevel = depthLevel;
         if(depthLevel < 1) {
             this.#boards = openBoard();
         }
+        this.#player = player;
+        this.#bestPlay = null;
+    }
+
+    bestBoard() {
+        let bestBoard = this;
+        let bestEvaluate = this.evaluate();
+        for(let i = 0; i < this.#boards.length; i++) {
+            let actualEvaluate = this.#boards[i].evaluate();
+            if(actualEvaluate >= bestEvaluate) {
+                bestEvaluate = actualEvaluate;
+                bestBoard = this.#boards[i];
+            }
+        }
+        return bestBoard;
     }
 
     openBoard() {
@@ -18,11 +33,17 @@ class Board {
     }
 
     evaluateDefense() {
-        //Pega a média dos valores individuais de defesa obtido na evaluateCountryDefenseAll()
-    }
-
-    evaluateCountryDefenseAll() {
-        //Pega a média da defesa com todas as fronteiras que não seja dele. (caso não tenha, retornará um valor constante)
+        //Pega a média dos valores individuais de defesa obtido na evaluateCountryDefense()
+        let sum = 0;
+        for(let i = 0; i < player.territoriesOwned.length; i++) {
+            let actual = player.territoriesOwned[i];
+            let tempSum = 0;
+            for(let j = 0; j < actual.neighbors.length; j++) {
+                tempSum += this.evaluateCountryDefense(actual, actual.neighbors[i]);
+            }
+            sum += tempSum/actual.neighbors.length;
+        }
+        return sum/player.territoriesOwned.length;
     }
 
     evaluateCountryDefense(from, to) {
@@ -39,7 +60,7 @@ class Board {
     }
 
     evaluateCountries() {
-        //Função que usa a quantidade de países
+        return this.player
     }
 
     evaluateContinents() {
